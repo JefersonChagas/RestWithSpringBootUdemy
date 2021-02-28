4156be0c0e80b6fe5bc6120969d4af2a8d338060
jefer <jeferson.estudo2017@gmail.com>
2021-02-28 14:16:28 -0300

    Conectando ao MYSQL

---
+++ 04 RestWithSpringBootUdemy Scaffold Eclipse  Connecting To MySQL/src/main/java/br/com/erudio/exception/ExceptionResponse.java
@@ -0,0 +1,38 @@
+package br.com.erudio.exception;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ExceptionResponse implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Date timestamp;
+	private String message;
+	private String detalls;
+	
+	public ExceptionResponse(Date timestamp, String message, String detalls) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.detalls = detalls;
+	}
+
+
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getDetalls() {
+		return detalls;
+	}
+	
+	
+	
+
+}
