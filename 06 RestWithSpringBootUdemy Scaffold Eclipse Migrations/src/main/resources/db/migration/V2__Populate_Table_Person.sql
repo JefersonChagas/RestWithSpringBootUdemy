e18cea8124f9f4a3148d8fa30f005616dff06127
jefer <jeferson.estudo2017@gmail.com>
2021-03-07 16:32:12 -0300

    Implementando Migrations

---
+++ 06 RestWithSpringBootUdemy Scaffold Eclipse Migrations/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO `person`(`id`,`first_name`,`last_name`, `address`, `gender`) VALUES
+					(1,'João','da Silva','São Paulo - SP - Brasil','M'),
+					(2,'Amanda','Cristina','Sorocaba - SP - Brasil','F'),
+					(3,'Debora','Simoes','Angatuba - SP - Brasil','F'),
+					(4,'julia','vaz','itapetininga - SP - Brasil','F'),
+					(5,'thiago','rodrigues','itapetininga - SP - Brasil','M'),
+					(6,'Carlos','TI','Angatuba - SP - Brasil','M');
+
